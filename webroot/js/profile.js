bc0780c89fb5f27aee92b42fd767da784dc9ff34
Cat Can <catcd_58@vnu.edu.vn>
2016-06-13 03:18:52 +0700

    Update database

--- webroot/js/profile.js
+++ webroot/js/profile.js
@@ -1,6 +1,6 @@
 function changePassword(myself) {
     newPassword = myself.elements.namedItem("newpassword").value;
-    $.post("/api/change-password", function( data ) {
+    $.post("/api/change-password", function(data) {
         alert(data);
     });
     return false;
