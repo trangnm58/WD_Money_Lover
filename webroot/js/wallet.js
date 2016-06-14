2c1354a559d67f8f1589b97910752203a3bdc276
fiser-luongNT7 <luongnt95@gmail.com>
2016-06-15 03:21:56 +0700

    format code js

--- webroot/js/wallet.js
+++ webroot/js/wallet.js
@@ -1,32 +1,40 @@
 var checkEmptyList = function() {
-	if ($("wallet-container > a").length == 0) {
-		$("wallet-container").append('<span>No transaction. Add more!</span>');
-	} else {
-		$("#wallet-container").find("span:first-child").remove();
-	}
+    if ($("wallet-container > a").length == 0) {
+        $("wallet-container").append(
+            '<span>No transaction. Add more!</span>');
+    } else {
+        $("#wallet-container").find("span:first-child").remove();
+    }
 }
 
-
 function addWallet() {
-	form = document.getElementById('add-wallet');
+    form = document.getElementById('add-wallet');
     name = $("#name").val(); //van null
     amount = $("#amount").val();
-    unit  = $("#unit").val();
+    unit = $("#unit").val();
     unit_name = $("#unit option:selected").text();
     description = $("#description").val();
     unit_name = $("#unit_id option:selected").text()
-
-    type = $("#type").val();    
-	$.post("/api/add-wallet", {type: type, name: name, amount: amount, unit: unit, description: description}, function(data, status) {		
-		$("#wallet-container").append(
-		'<a class="list-group-item total each-wallet" id ="' + data["id"] + '"  data-toggle="modal" data-target="#manageWallet"> <i class=" fa ' + type +  ' fa-2x " aria-hidden="true" > </i> <div class="each-wallet-info"> <strong>' + name + '</strong> <p>' + unit_name +' '+ amount+ ' </p>  </div> </a>'
-		);
+    type = $("#type").val();
+    $.post("/api/add-wallet", {
+        type: type,
+        name: name,
+        amount: amount,
+        unit: unit,
+        description: description
+    }, function(data, status) {
+        $("#wallet-container").append(
+            '<a class="list-group-item total each-wallet" id ="' +
+            data["id"] +
+            '"  data-toggle="modal" data-target="#manageWallet"> <i class=" fa ' +
+            type +
+            ' fa-2x " aria-hidden="true" > </i> <div class="each-wallet-info"> <strong>' +
+            name + '</strong> <p>' + unit_name + ' ' + amount +
+            ' </p>  </div> </a>');
     });
-	backToList();
-	return false;
+    backToList();
+    return false;
 }
-
-
 
 function saveEditWallet() {
     form = document.getElementById('editWallet');
@@ -35,27 +43,28 @@ function saveEditWallet() {
     unit_id = form.elements.namedItem('gender').value;
     description = form.elements.namedItem('_description').value;
     type = form.elements.namedItem('_type').value;
-
-    data = {name: name, amount: amount, unit_id:unit_id, description:description, type:type};
-    $.post('/api/update-wallet', data, function (data) {
+    data = {
+        name: name,
+        amount: amount,
+        unit_id: unit_id,
+        description: description,
+        type: type
+    };
+    $.post('/api/update-wallet', data, function(data) {
         if (data == "SUCCESS") {
             alert("UPDATE SUCCESS");
         } else {
-        	alert("UPDATE FAILED");
+            alert("UPDATE FAILED");
         }
     });
 }
 
 function getWalletInfo(myself) {
-	$.post("/api/get-wallet-Info", function( data ) {
-       
-    });
-}
-function onclick() {
-	
+    $.post("/api/get-wallet-Info", function(data) {});
 }
 
-var backToList = function() {	
-	// show list
-	$(".included-in-total").toggle();
+function onclick() {}
+var backToList = function() {
+    // show list
+    $(".included-in-total").toggle();
 }
